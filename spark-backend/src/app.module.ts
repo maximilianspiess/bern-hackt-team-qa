e4efce74e055033bff06cc06a0be4fbd30af4985
Maximilian Spiess <maximilian.spiess@proton.me>
2025-08-23 01:04:44 +0200

    feat: add DB connection and user endpoints

--- spark-backend/src/app.module.ts
+++ spark-backend/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {User} from "./users/entities/user.entity";
 
 @Module({
-  imports: [],
+  imports: [
+      UsersModule,
+      TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5433,
+        username: 'spark_user',
+        password: 'sp4rk',
+        database: 'spark',
+        entities: [User],
+        synchronize: true
+      })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ spark-backend/src/users/users.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UsersService {
+  create(createUserDto: CreateUserDto) {
+
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
