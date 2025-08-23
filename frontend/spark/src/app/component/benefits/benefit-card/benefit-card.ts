9e714dd9f6865988ac4c085decf708921e0cc475
Maximilian Spiess <maximilian.spiess@proton.me>
2025-08-23 20:21:15 +0200

    feat: add benefit page with confirmation dialog

--- frontend/spark/src/app/component/benefits/benefit-card/benefit-card.ts
+++ frontend/spark/src/app/component/benefits/benefit-card/benefit-card.ts
@@ -1,6 +1,9 @@
-import {Component, HostBinding, Input, signal} from '@angular/core';
+import {Component, HostBinding, Input} from '@angular/core';
 import {BenefitEntity} from '../../../model/BenefitEntity';
 import {MatCard, MatCardContent} from '@angular/material/card';
+import {MatDialog} from '@angular/material/dialog';
+import {ConfirmDialogComponent} from './confirm-dialog.component';
+import {BenefitService} from '../benefit-service';
 
 @Component({
   selector: 'app-benefit-card',
@@ -14,4 +17,21 @@ import {MatCard, MatCardContent} from '@angular/material/card';
 export class BenefitCard {
   @HostBinding('class.benefit-card') class: boolean = true;
   @Input() benefit!: BenefitEntity;
+
+  constructor(private dialog: MatDialog,
+              private benefitService: BenefitService){}
+
+  openDialog(){
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      data: {title: 'Do you want to redeem ' + this.benefit.title + '?', price: this.benefit.sparkPrice}
+    })
+
+    dialogRef.afterClosed().subscribe(res => {
+      if (res){
+        this.benefitService.redeemBenefit(this.benefit.id);
+      } else {
+        // do nothing
+      }
+    })
+  }
 }
--- frontend/spark/src/app/model/BenefitEntity.ts
+++ frontend/spark/src/app/model/BenefitEntity.ts
@@ -2,16 +2,22 @@ import {CompanyEntity} from './CompanyEntity';
 
 export class BenefitEntity{
   id: string;
+  title: string;
+  description: string;
   discount: number;
   expirationDate: Date;
   sparkPrice: number;
+  imageUlr: string;
   company: CompanyEntity;
 
-  constructor(id: string, discount: number, expirationDate: Date, sparkPrice: number, company: CompanyEntity) {
+  constructor(id: string, title: string, description: string, discount: number, expirationDate: Date, sparkPrice: number, imageUrl: string, company: CompanyEntity) {
     this.id = id;
+    this.title = title;
+    this.description = description;
     this.discount = discount;
     this.expirationDate = expirationDate;
     this.sparkPrice = sparkPrice;
+    this.imageUlr = imageUrl;
     this.company = company;
   }
 }
