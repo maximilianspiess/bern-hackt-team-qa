8696e2ef3db7025992b6a22af4b0d3cda1566420
Quagamole <61179374+Quagamole@users.noreply.github.com>
2025-08-23 04:55:53 +0200

    login and init router config

--- frontend/spark/src/app/app.routes.ts
+++ frontend/spark/src/app/app.routes.ts
@@ -1,3 +1,7 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {Login} from './component/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: Login, title: "Spark"},
+  {path: 'login', component: Login, title: "Login | Spark"},
+];
--- frontend/spark/src/app/app.ts
+++ frontend/spark/src/app/app.ts
@@ -1,5 +1,5 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +7,6 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
+
 export class App {
-  protected readonly title = signal('spark');
 }
